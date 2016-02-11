2653de2f2ccf416db0869c0e0b729f5cc27a6414
Léonard Blier <leonard.blier@gmail.com>
2016-02-11 12:08:23 +0100

    bug solving

--- script.py
+++ script.py
@@ -36,6 +36,7 @@ def grid_search():
     img = 'einstein.jpg'
     styles_path = 'styles/abstract/'
     style = 'comp-vasarely1.jpg'
+    results_path = 'results/'
     
     tv_array = ([0.1, 0.5, 1., 2., 5., 10.])
     cont_array = ([0.001, 0.01, 0.02, 0.05, 0.1, 0.5, 1.])
